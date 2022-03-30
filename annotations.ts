634584bd82586979cf608540474a59f9b587096f
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-03-30 21:26:12 +0000

    Replace with some examples of TypeScript inferance

--- annotations.ts
+++ annotations.ts
@@ -1,10 +1,11 @@
 // Primative types (string, number, boolean, null, undefined, symbol)
 let counter: number;
-let firstName: string = 'John';
-const active: boolean = true;
+let firstName = 'John'; // Infer to type string
+const active = true; // Infer to type boolean
 
 // Object types (array)
-let names: string[] = ['John', 'Jane', 'Peter', 'David', 'Mary'];
+let names = ['John', 'Jane', 'Peter', 'David', 'Mary']; // Infer to type string[]
+let items = [0, 1, null, 'Hi']; // Infer to type (number | string)[]
 
 // Object types (object)
 let person: {
@@ -23,3 +24,7 @@ let greeting : (name: string) => string;
 greeting = function (name: string) {
   return `Hi ${name}`;
 };
+
+function increment(counter: number) : number {
+  return counter++;
+}
