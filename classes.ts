52162982153ad8f580a073f1bd8109c7c769fc52
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-04-15 00:37:53 -0400

    Add example of readonly and using a class

--- classes.ts
+++ classes.ts
@@ -1,14 +1,18 @@
 // Access modifiers: private, protected, public (default)
-// Private = can access only within the class
-// Protected = can access within the class and subclasses (that inherits the class)
+// private = can access only within the class
+// protected = can access within the class and subclasses (that inherits the class)
+// readonly = class property cannot be reassigned a new value once a value is set
 class Person {
-  constructor(protected ssn: string, private firstName: string, private lastName: string) {
+  constructor(protected ssn: string, private firstName: string, private lastName: string, readonly birthDate: Date) {
       this.ssn = ssn;
       this.firstName = firstName;
       this.lastName = lastName;
+      this.birthDate = birthDate;
   }
 
   getFullName(): string {
       return `${this.firstName} ${this.lastName}`;
   }
 }
+
+let person = new Person('153-07-3130', 'John', 'Doe', new Date(1990, 12, 25));
